c53100c44230bf91170f25dbb0e891e92868a135
Thanh Tung <thanhtung031093@gmail.com>
2022-08-10 14:26:30 +0700

    Add bai 8-9

---
+++ Bai 8-9/main.js
@@ -0,0 +1,14 @@
+
+var a = 83587345;
+var b = 487365;
+var c = a + b ;
+var d = a - b;
+var e = (a + b) / 2 ;
+var f = a % b;
+var g = a / b;
+console.log(d);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
