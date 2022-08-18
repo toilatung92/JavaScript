bab9811820af259a7a478b86edec24167ff87e0f
Thanh Tung <thanhtung031093@gmail.com>
2022-08-18 22:01:48 +0700

    Add bai 23

---
+++ Bai23- Mảng/main.js
@@ -0,0 +1,22 @@
+
+// Tạo mảng và kiểm tra //
+var a = [
+    'A',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    'j',
+    'q',
+    'k',
+]
+console.log(a);
+
+//Kiem tra//
+console.log(typeof a) 
+console.log(Array.isArray(a)); // Kiem tra Array//
+// Truy xuất mảng//
+console.log(a.length);// Đếm độ dài cảu mảng//
+console.log(a[7]);// Lấy phần tử theo index//
