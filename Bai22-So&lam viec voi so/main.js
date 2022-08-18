075dc0bff57b7d73ce35e69ff2f3b2f77ed22a5c
Thanh Tung <thanhtung031093@gmail.com>
2022-08-18 21:43:10 +0700

    Add bai 22

---
+++ Bai22-So&lam viec voi so/main.js
@@ -0,0 +1,15 @@
+
+// Tạo giá trị nummber và kiểm tra data type//
+var a = 1;
+var b = 5;
+
+console.log(typeof a);
+// Làm việc với number//
+// Keyword: Javarscrip number methods//
+//To string (Chuyển đổi kiểu number -> String)
+console.log(a.toString());
+// To Fixed ( Làm tròn số thập phân)
+var pi = 3.14;
+console.log(pi.toFixed());
+var c = 34.67467456764565;
+console.log(c.toFixed(2));
